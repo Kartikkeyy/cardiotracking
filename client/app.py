34a3db1ccd58be5474cffbaf9c301b31a83fd26e
kartikkeyy <kartikwork0805@gmail.com>
2026-02-10 16:47:26 +0530

    client update

--- client/app.py
+++ client/app.py
@@ -7,7 +7,6 @@ from components.chatUI import render_chat
 st.set_page_config(page_title="AI Medical Assistant",layout="wide")
 st.title(" 🩺 Medical Assistant Chatbot")
 
-
 render_uploader()
 render_chat()
 render_history_download()
--- client/config.py
+++ client/config.py
@@ -1,2 +1,3 @@
 # API_URL="http://127.0.0.1:8000"
+
 API_URL="https://medical-assistant-gv85.onrender.com"
